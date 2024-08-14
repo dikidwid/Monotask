493854565ee682a0ff5319cbbb4463b998f5e324
Diki Dwi Diro <dikidwid0@gmail.com>
2024-08-14 22:22:46 +0700

    Initial Commit

---
+++ Monotask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monotask
+//
+//  Created by Diki Dwi Diro on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monotask/MonotaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonotaskApp.swift
+//  Monotask
+//
+//  Created by Diki Dwi Diro on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MonotaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
